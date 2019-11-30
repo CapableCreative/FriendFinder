1d44540dd2b5e79d6e79528f138c7937b3b518ab
Stephen Fox <stephen@capablecreative.com>
2019-11-30 16:02:54 -0600

    Server logic, routes, score comparisons - SLF

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,37 @@
-$("#submit").on("click", function(){
-    
-});
+var friends = [
+{
+    name: "Tim Analytical",
+    photo: 'https://media.giphy.com/media/FrbYVfk9Ld44g/giphy.gif',
+    scores: [10,9,8,7,6,5,4,3,2,1]
+},
+{
+    name: "Johnathon Arrogant",
+    photo: 'https://media.giphy.com/media/OoOlmRkRtiYwM/giphy.gif',
+    scores: [10,9,8,7,6,5,4,3,2,1]
+},
+{
+    name: "David Pragmatic",
+    photo: 'https://media.giphy.com/media/7JmwRN1wQLPyZOQArQ/giphy.gif',
+    scores: [10,9,8,7,6,5,4,3,2,1]
+},
+{
+    name: "Jennifer Passionate",
+    photo: 'https://media.giphy.com/media/l0MYJEC0i1nHeCi8U/giphy.gif',
+    scores: [10,9,8,7,6,5,4,3,2,1]
+},
+{
+    name: "Ken Fairweather",
+    photo: 'https://media.giphy.com/media/R2FjNX1KTECdi/giphy.gif',
+    scores: [10,9,8,7,6,5,4,3,2,1]
+},
+{
+    name: "Micky McReynolds",
+    photo: 'https://media.giphy.com/media/IzyI9jLVqDvB6/giphy.gif',
+    scores: [10,9,8,7,6,5,4,3,2,1]
+}
+
+];
+
+module.exports = friends;
+
+// $("#submit").on("click", function(){  });
