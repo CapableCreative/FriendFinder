6ff75d5067c7b7536fb76d1ad6825a81cf678e5f
Stephen Fox <stephen@capablecreative.com>
2019-11-30 22:17:21 -0600

    Update to styling - SLF

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:"application/vnd.api+json"}));
+app.use('/img',express.static(path.join(__dirname, 'app/public/img')));
+app.use('/css',express.static(path.join(__dirname, 'app/public/css')));
 
 app.use(express.static('app/public'));
 
